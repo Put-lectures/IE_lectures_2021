5e23bb3a7887907c726f6c5795caa29b9d05406b
Piotr Kaczmarek <piokac@gmail.com>
2021-10-15 10:08:08 +0200

    Lectur 3 - loops and control flow

---
+++ L2_loops/for_loop/main.cpp
@@ -0,0 +1,53 @@
+﻿#include <fstream>
+#include <iostream>
+#define ARRAY_SIZE 5
+using namespace std;
+
+int main()
+{
+    //    For loop demo
+    double array[ARRAY_SIZE];
+    for (int i = 0; i < ARRAY_SIZE; i += 1) {
+        cout << "Enter an element # " << i << endl;
+        cin >> array[i];
+    }
+    //mean value
+    double acc = 0;
+    for (int i = 0; i < ARRAY_SIZE; i += 1) {
+        acc += array[i];
+        cout << array[i] << " ";
+    }
+
+    cout << acc / ARRAY_SIZE;
+
+    //while loop demo
+    fstream file("./data.txt", ios::in);
+
+    while (!file.eof()) {
+        int val;
+        file >> val;
+        cout << val << " ";
+    }
+    //do while demo
+    char answer;
+    do {
+        cout << "Do you want to continue (y/n)";
+        cin >> answer;
+
+    } while (!(answer == 'y' || answer == 'n'));
+    //answer != 'y' &&answer != 'n'
+
+    if (answer == 'y') {
+        cout << "good bye";
+    }
+
+    //Numerical errors
+    float time = 0, dt = 0.1;
+
+    for (int i = 0; i < 1000000; i++) {
+        time += dt;
+    }
+    cout << time;
+
+    return 0;
+}
---
+++ l2_printf_formatting/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <cstring>
+
+#include <iostream>
+#define pi 3.141597
+using namespace std;
+
+int main()
+{
+    cout << "are you sure to exit (y/n)";
+    char a;
+    cin >> a;
+
+    //    if (a == 'y' || a == 'Y') {
+    //        cout << "Good bye";
+    //    } else {
+    //        if (a == 'n' || a == 'N')
+    //            cout << "continue";
+    //        else {
+    //            cout << "wrong answer";
+    //        }
+    //    }
+    switch (a) {
+    case 'y':
+    case 'Y':
+        cout << "Good bye";
+        break;
+    case 'n':
+    case 'N':
+        cout << "continue";
+        break;
+    default:
+        cout << "wrong answer";
+    }
+
+    return 0;
+}
